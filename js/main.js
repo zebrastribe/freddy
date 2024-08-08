119664186986bd21ed6e223f58779d97f7a93513
zebrastribe <jesper@kahrnielsen.dk>
2024-08-08 21:36:31 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      console.log("Latitude: " + position.coords.latitude);
+      console.log("Longitude: " + position.coords.longitude);
+    },
+    function(error) {
+      console.error("Error Code = " + error.code + " - " + error.message);
+    }
+  );
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
