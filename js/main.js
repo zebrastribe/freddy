05a0defee28afb07c884ec4f5c2172d270cf4150
zebrastribe <jesper@kahrnielsen.dk>
2024-08-08 21:54:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-function getCoordinates() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        console.log("Latitude: " + position.coords.latitude);
-        console.log("Longitude: " + position.coords.longitude);
-      },
-      function(error) {
-        console.error("Error Code = " + error.code + " - " + error.message);
-      }
-    );
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
+function initMap(lat, lng) {
+  var map = L.map('map').setView([lat, lng], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([lat, lng]).addTo(map)
+    .bindPopup('You are here.')
+    .openPopup();
 }
 
-// Call the function to get coordinates
-getCoordinates();
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      console.log("Latitude: " + lat);
+      console.log("Longitude: " + lng);
+      initMap(lat, lng);
+    },
+    function(error) {
+      console.error("Error Code = " + error.code + " - " + error.message);
+    }
+  );
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
