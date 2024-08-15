65b77595d170f2d7523680688d919cbcc4a9b473
zebrastribe <jesper@kahrnielsen.dk>
2024-08-15 19:37:51 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLFO04OQgD6LjYdYlrEXb73THTp5H0Ss",
+  authDomain: "tracker-6a648.firebaseapp.com",
+  projectId: "tracker-6a648",
+  storageBucket: "tracker-6a648.appspot.com",
+  messagingSenderId: "789878332530",
+  appId: "1:789878332530:web:9bd999d86df0fe9caef6eb",
+  measurementId: "G-XMHHKFJ9QW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Sign in anonymously
+signInAnonymously(auth).catch((error) => {
+  console.error("Error signing in anonymously: ", error);
+});
+
+// Function to store data on button click
+document.getElementById('myButton').addEventListener('click', () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      addDoc(collection(db, "clicks"), {
+        timestamp: serverTimestamp(),
+        userId: user.uid
+      })
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+    } else {
+      console.error("User is not authenticated");
+    }
+  });
+});
