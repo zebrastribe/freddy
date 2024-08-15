e18f0b6f1120c9315697814e7323bc2beee58615
zebrastribe <jesper@kahrnielsen.dk>
2024-08-15 21:34:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-// app.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-// Import Firebase services
-import { db, auth, onAuthStateChanged } from './index.html';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwLFO04OQgD6LjYdYlrEXb73THTp5H0Ss",
+  authDomain: "tracker-6a648.firebaseapp.com",
+  projectId: "tracker-6a648",
+  storageBucket: "tracker-6a648.appspot.com",
+  messagingSenderId: "789878332530",
+  appId: "1:789878332530:web:9bd999d86df0fe9caef6eb",
+  measurementId: "G-XMHHKFJ9QW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Sign in anonymously
+signInAnonymously(auth).catch((error) => {
+  console.error("Error signing in anonymously: ", error);
+});
 
 // React component
 class App extends React.Component {
@@ -17,8 +40,8 @@ class App extends React.Component {
 
   handleClick = () => {
     if (this.user) {
-      db.collection("clicks").add({
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      addDoc(collection(db, "clicks"), {
+        timestamp: serverTimestamp(),
         userId: this.user.uid
       })
       .then(() => {
