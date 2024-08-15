c503ece1a598c3ff234f783e144e1b4f2b85b74c
zebrastribe <jesper@kahrnielsen.dk>
2024-08-15 21:23:00 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,12 @@
 // app.js
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBwLFO04OQgD6LjYdYlrEXb73THTp5H0Ss",
-  authDomain: "tracker-6a648.firebaseapp.com",
-  projectId: "tracker-6a648",
-  storageBucket: "tracker-6a648.appspot.com",
-  messagingSenderId: "789878332530",
-  appId: "1:789878332530:web:9bd999d86df0fe9caef6eb",
-  measurementId: "G-XMHHKFJ9QW"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const analytics = firebase.analytics();
-const db = firebase.firestore();
-const auth = firebase.auth();
-
-// Sign in anonymously
-auth.signInAnonymously().catch((error) => {
-  console.error("Error signing in anonymously: ", error);
-});
+// Import Firebase services
+import { db, auth, onAuthStateChanged } from './index.html';
 
 // React component
 class App extends React.Component {
   componentDidMount() {
-    auth.onAuthStateChanged((user) => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
         this.user = user;
       } else {
