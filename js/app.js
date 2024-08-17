9c97becd8dd31751646a2540889e9ec6acf628d8
zebrastribe <jesper@kahrnielsen.dk>
2024-08-17 21:27:44 +0200

    sad

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function getUrlParameter(name) {
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
-// Function to validate token
+// Function to validate token.
 async function validateToken(token) {
   try {
     const tokenDoc = await getDoc(doc(db, 'tokens', token));
